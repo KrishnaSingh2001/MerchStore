f0404edbcef5e7c2a081cb64f7cde2e7a4e4437e
Krishna Singh <krishnasingh@Krishnas-MacBook-Air.local>
2024-08-24 22:15:33 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import {
+/*import {
   Facebook,
   Instagram,
   MailOutline,
@@ -7,6 +7,8 @@ import {
   Room,
   Twitter,
 } from "@material-ui/icons";
+
+
 import styled from "styled-components";
 import { mobile } from "../responsive";
 
@@ -141,4 +143,142 @@ const Footer = () => {
   );
 };
 
+export default Footer;*/
+
+
+
+
+
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <FooterSection>
+          <SectionTitle>About Us</SectionTitle>
+          <p>
+            We are a leading e-commerce platform offering a wide range of products from various categories.
+            Our mission is to provide the best shopping experience to our customers.
+          </p>
+        </FooterSection>
+
+        <FooterSection>
+          <SectionTitle>Quick Links</SectionTitle>
+          <ul>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/faq">FAQ</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+            <li><a href="/returns">Returns & Exchanges</a></li>
+          </ul>
+        </FooterSection>
+
+        <FooterSection>
+          <SectionTitle>Customer Service</SectionTitle>
+          <ul>
+            <li><a href="/support">Support</a></li>
+            <li><a href="/shipping">Shipping & Delivery</a></li>
+            <li><a href="/order-tracking">Order Tracking</a></li>
+            <li><a href="/privacy">Privacy Policy</a></li>
+            <li><a href="/terms">Terms & Conditions</a></li>
+          </ul>
+        </FooterSection>
+
+        <FooterSection>
+          <SectionTitle>Follow Us</SectionTitle>
+          <SocialIcons>
+            <li><a href="https://facebook.com" className="facebook">Facebook</a></li>
+            <li><a href="https://twitter.com" className="twitter">Twitter</a></li>
+            <li><a href="https://instagram.com" className="instagram">Instagram</a></li>
+            <li><a href="https://linkedin.com" className="linkedin">LinkedIn</a></li>
+          </SocialIcons>
+        </FooterSection>
+
+        <FooterSection>
+          <SectionTitle>Contact Us</SectionTitle>
+          <p><i className="fas fa-phone"></i> +1 234 567 890</p>
+          <p><i className="fas fa-envelope"></i> support@shoppingwebsite.com</p>
+        </FooterSection>
+      </FooterContent>
+
+      <FooterBottom>
+        <p>&copy; {new Date().getFullYear()} Shopping Website. All rights reserved.</p>
+      </FooterBottom>
+    </FooterContainer>
+  );
+};
+
 export default Footer;
+
+const FooterContainer = styled.footer`
+  background-color: #333;
+  color: #fff;
+  padding: 40px 0;
+  text-align: left;
+  font-size: 14px;
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const FooterSection = styled.div`
+  flex: 1;
+  margin: 20px;
+`;
+
+const SectionTitle = styled.h2`
+  margin-bottom: 20px;
+  font-size: 18px;
+  color: #f0f0f0;
+`;
+
+const SocialIcons = styled.ul`
+  display: flex;
+  gap: 10px;
+
+  li a {
+    color: #bbb;
+    font-size: 20px;
+    transition: color 0.3s;
+  }
+
+  li a:hover {
+    color: #fff;
+  }
+`;
+
+const FooterBottom = styled.div`
+  text-align: center;
+  padding: 10px 0;
+  border-top: 1px solid #444;
+
+  p {
+    margin: 0;
+  }
+`;
+
+const Ul = styled.ul`
+  list-style: none;
+  padding: 0;
+
+  li {
+    margin-bottom: 10px;
+  }
+
+  li a {
+    color: #bbb;
+    text-decoration: none;
+    transition: color 0.3s;
+  }
+
+  li a:hover {
+    color: #fff;
+  }
+`;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,4 @@
+/*
 import {
   FavoriteBorderOutlined,
   SearchOutlined,
@@ -84,6 +85,97 @@ const Product = ({ item }) => {
         </Icon>
         <Icon>
           <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
+
+export default Product;
+
+*/
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import { Link } from "react-router-dom";
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  z-index: 2;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
+
+const Product = ({ item }) => {
+  return (
+    <Container>
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <i className="fas fa-shopping-cart"></i>
+        </Icon>
+        <Icon>
+          <Link to={`/product/${item._id}`}>
+            <i className="fas fa-search"></i>
+          </Link>
+        </Icon>
+        <Icon>
+          <i className="fas fa-heart"></i>
         </Icon>
       </Info>
     </Container>
