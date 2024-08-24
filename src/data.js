fe63372f6969be9fd0a9c02e0f02b434ff22d14d
KrishnaSingh2001 <136974650+KrishnaSingh2001@users.noreply.github.com>
2024-08-24 18:33:15 +0000

    fix: image rendering in deployment

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 export const sliderItems = [
     {
       id: 1,
-      //img: "https://images.unsplash.com/photo-1718784560751-a4423eca657d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fHww",
-      img:"src/assets/banner2.jpg",
+      img: "https://images.unsplash.com/photo-1718784560751-a4423eca657d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fHww",
+      // img:"public/static/images/banner2.jpg",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
     },
     {
       id: 2,
-      //img: "https://i.ibb.co/DG69bQ4/2.png",
-      img:"src/assets/banner3.jpg",
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      // img:"public/static/images/banner3.jpg",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
@@ -18,7 +18,7 @@ export const sliderItems = [
     {
       id: 3,
       //img: "https://i.ibb.co/cXFnLLV/3.png",
-      img:"src/assets/banner.jpg",
+      img:"public/static/images/banner.jpg",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
@@ -29,21 +29,21 @@ export const sliderItems = [
     {
       id: 1,
       //img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      img:"src/assets/categoryWomen.jpg",
+      img:"public/static/images/categoryWomen.jpg",
       title: "WOMEN",
       cat:"women"
     },
     {
       id: 2,
       //img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      img:"src/assets/categoryMen.jpg",
+      img:"public/static/images/categoryMen.jpg",
       title: "MEN",
       cat:"men"
     },
     {
       id: 3,
       //img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      img:"src/assets/categoryKids.jpg",
+      img:"public/static/images/categoryKids.jpg",
       title: "KIDS",
       cat:"kids"
     },
