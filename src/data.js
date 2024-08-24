ffc7b2fb9944d5ade2be9e2fc24b9c088650f7b3
KrishnaSingh2001 <136974650+KrishnaSingh2001@users.noreply.github.com>
2024-08-24 18:37:50 +0000

    fixing: image

--- src/data.js
+++ src/data.js
@@ -17,8 +17,8 @@ export const sliderItems = [
     },
     {
       id: 3,
-      //img: "https://i.ibb.co/cXFnLLV/3.png",
-      img:"public/static/images/banner.jpg",
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      // img:"public/static/images/banner.jpg",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
@@ -28,22 +28,22 @@ export const sliderItems = [
   export const categories = [
     {
       id: 1,
-      //img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      img:"public/static/images/categoryWomen.jpg",
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      // img:"public/static/images/categoryWomen.jpg",
       title: "WOMEN",
       cat:"women"
     },
     {
       id: 2,
-      //img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      img:"public/static/images/categoryMen.jpg",
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      // img:"public/static/images/categoryMen.jpg",
       title: "MEN",
       cat:"men"
     },
     {
       id: 3,
-      //img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      img:"public/static/images/categoryKids.jpg",
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      // img:"public/static/images/categoryKids.jpg",
       title: "KIDS",
       cat:"kids"
     },
